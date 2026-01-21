25ffc7c387ee03d23c44c1e75f34018b328e436a
Babar Hussain <hussainbabar171@gmail.com>
2026-01-21 16:01:31 +0530

    added python variables practice files

---
+++ variables.py
@@ -0,0 +1,19 @@
+# Python Basics
+# Vairables 
+a = 1
+print(a)
+
+abc = 12
+print(abc)
+
+
+a,b,c = 1,2,3
+print(a,b,c)
+
+x=y=z= 100
+print(x,y,z)
+print(x)
+print(x,y)
+
+# find the memory address of variable
+print(id(a))

bfa269e248ddc92a6d5989b3b96387c1744fbd26
Babar Hussain <hussainbabar171@gmail.com>
2026-01-22 18:52:53 +0530

    Python Strings

---
+++ Strings.py
@@ -0,0 +1,22 @@
+str = "PythonP"
+print(str)
+print(type(str))
+print(str[3])
+print(str[::-1])
+print(str[4:5])
+print(str[-3: -1])
+print(str[-5:-2:1])
+
+print(len(str))
+
+str1 = "Hello World"
+
+print(str1.count('l'))
+print(str.count('P'))
+
+print(str.upper())
+print(str.capitalize())
+print(str.lower())
+print(str.replace('n','N'))
+print(str.split('h'))
+print(str.find('p'))

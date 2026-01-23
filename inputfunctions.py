cb0862784de09569a6ac6554861521cc4389f574
Babar Hussain <hussainbabar171@gmail.com>
2026-01-23 11:43:52 +0530

    Python Input function

---
+++ inputfunctions.py
@@ -0,0 +1,4 @@
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
+print(first_number + second_number)  # This will concatenate the inputs as strings
+
